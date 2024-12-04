4d1e489c75955e18db1b334d6a49bbf4ffea9322
Sauharda Meher Shrestha <98987144+Sauharda1184@users.noreply.github.com>
2024-12-04 00:25:30 +0000

    Commit

--- WarehouseFSM/Product.java
+++ WarehouseFSM/Product.java
@@ -1,121 +1,30 @@
-/**************************************************************************
-Product Class 
- Create product object. Each product has a description, id,
- Sale Price and amount in stock.
-***************************************************************************/
+package WarehouseSystem;
+public class Product {
+    // Private fields
+    private String productID;
+    private String name;
+    private Float price;
+    private Integer quantity;
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-public class Product implements Serializable {
-  private static final long serialVersionUID = 1L;
-  
-  private String name;
-  private int id;
-  private int inStock;
-  private double salePrice;
-  private List<WaitListProduct> waitList = new ArrayList<WaitListProduct>();
-
-//----------Contructor-------------------
-  public Product(){
-    id = ProductIdServer.instance().getId();
-    name = "";
-    salePrice = 0;
-    inStock = 0;
-  }
-
-//---------accessors------------------
-  public int getProductNumber(){
-    return id;   }
-
-  public String getName(){
-    return name;  }
-
-  public double getSalePrice(){
-    return salePrice;     }
-
-  public int getStock(){
-	return inStock;  }
-
-  
-  public String getData(){
-    String data = "";
-    data += name;
-    data += "\n\tId: " + id;
-    data += "\n\tSale Price:     ";
-    data += salePrice;
-    data += "\n\tStock:          ";
-    data += inStock;
-    data += "\n\tNumber of waitlisted orders: ";
-    data += waitList.size();
-    return data;
+    // Constructor
+    public Product (String productID, String name, Float price, Integer quantity) {
+        this.productID = productID;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
     }
-    
-    
-//-----------mutators----------------
-  public void setName(String input){
-    name = input;  }
-    
-  public void setSalePrice(double input){
-    salePrice = input;     }
 
-    
-  public void setData(){
-    System.out.println("To use data Setter input data members in this order\n\tString name\n\tdouble salePrice");
+    // Getters
+    public String getID() {return productID;}
+    public String getName() {return name;}
+    public Float getPrice() {return price;}
+    public Integer getQuantity() {return quantity;}
+
+    // Operations
+    public void modifyQuantity(Integer amount) {
+        this.quantity += amount;
     }
-    
-  public void setData(String n, double sp, int st){
-    name            =    n;
-    salePrice       =   sp;
-    inStock 		=   st;
-
+    public void setQuantity(Integer amount) {
+        this.quantity = amount;
     }
-    
-    
-//-------------removeStock------------
-// Remove specified amount of stock
-//------------------------------------
-  public void removeStock(int r){
-	inStock -= r;
-  }//end removeStock
-  
-//-----------AddItem-----------------
-// Add Item to Stock from Shipment
-//-----------------------------------
-  public void addStock(int quantity){
-	  inStock += quantity;
-  }//end addShippedItem
-  
-  
-//--------WaitListProduct--------------
-  public void waitListProduct(Order o, int quantity){
-	waitList.add(new WaitListProduct(o, quantity));
-  }//end waitListItem
-
-    
-//------WaitList Iterator-----------------
-  public Iterator getWaitList(){
-	  return waitList.iterator();	  
-  }//end getWaitList
-  
-//-----------FulfillWaitlist---------------------
-// Process item from waitlist then remove it
-//-------------------------------------------------
-  public void finishWaitList(WaitListProduct item){
-     removeStock(item.getQuantity());
-	  item.process();
-	  waitList.remove(item);
-  }//end finishWaitlist
-    
-//-------------toString()----------------
-  public String toString(){
-     return getData();  }
-    
-	  
-}//end Product Class
-
-
-
-
-
+}
--- WarehouseFSM/ProductList.java
+++ WarehouseFSM/ProductList.java
@@ -1,177 +1,44 @@
-/********************************************************************
-  Product List Class
-        List of products in warehouse system
-**********************************************************************/
-
+package WarehouseSystem;
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-public class ProductList implements Serializable{
-  private static final long serialVersionUID = 1L;
- 
-  private List<Product> products = new LinkedList<Product>();
-  private static ProductList pList;
+public class ProductList {
+    // Private fields
+    private static ProductList productList;
+    private List<Product> products = new LinkedList<Product>();
 
-    
-//----------Constructor-------------
-  public ProductList(){    }
+    // Constructor
+    private ProductList(){}; // Private to enforce singleton
 
-    
-//----------instance()--------------------
-  public static ProductList instance() {
-  	if(pList == null){
-  		return (pList = new ProductList());
-  	} else{
-  		return pList;
-  	}//end ifelse
-  }//end instance()
-
-    
-//----------accessor---------------------
-   public Iterator getProduct(){
-     return products.iterator();  }
-
-//----------size-------------
-// size of product List
-//---------------------------
-    public int size(){
-        return products.size();
-    }//end size()
-    
-//-------------displayList-----------------
-// Display Product List
-//-----------------------------------------
-    public void displayList(){
-        for(Iterator current = products.iterator(); current.hasNext();){
-            Product P = (Product) current.next();
-            System.out.println(P.getData());
+    // Operations
+    public static ProductList instance() { // Creation method
+        if (productList == null) {
+            return (productList = new ProductList());
         }
-    }//end DisplayList
-    
-//-----------Add Product--------------------
-// Add product to product List
-//------------------------------------------
-  public void addProduct(String d, double sp, int st){
-    Product P = new Product();
-    P.setData(d, sp, st);
-    products.add(P);
-  }//end addProduct
-    
- //--------removeProduct--------------------
-// Remove product from Product List
-//-------------------------------------------
-  public void removeProduct(int PID){
-    int i = 0;
-    for(Iterator current = products.iterator(); current.hasNext();){
-      i++;
-      Product P = (Product) current.next();
-      if(P.getProductNumber() == PID){
-        products.remove(i);
-      }//endif
-    }//endfor
-  }//end removeProduct
-    
-    
-//----------------findProduct----------------------
-// Find product in product List by product id
-// returns Null if id not found in list
-//-----------------------------------------------
-  public Product findProduct(int id){
-    for(Iterator current = products.iterator(); current.hasNext();){
-      Product P = (Product) current.next();
-      if(P.getProductNumber() == id){
-        return P;
-      }
+        else {
+            return productList;
+        }
     }
-    return null;
-  }
-    
-//---------------writeObject---------------------
-  private static void writeObject(java.io.ObjectOutputStream output) {
-	try{
-		output.defaultWriteObject();
-		output.writeObject(pList);
-	} catch(IOException ioe){
-		ioe.printStackTrace();
-	} //end try-catch block
-  }//end writeObject
 
-//---------------readObject------------------------
-  private static void readObject(java.io.ObjectInputStream input){
-	try{
-		if(pList != null)
-			return;
-		else{
-			input.defaultReadObject();
-			if(pList == null){
-				pList = (ProductList) input.readObject();
-			} else {
-				input.readObject();
-			}//end if-else
-		}//end if-else
-	} catch (IOException ioe){
-		ioe.printStackTrace();
-	} catch(ClassNotFoundException cnfe) {
-		cnfe.printStackTrace();
-	}//end try-catch block
-  }//end readObject
+    public boolean addProduct(Product product) {
+        products.add(product);
+        return true;
+    }
 
-//-----------toString()------------------------
-  public String toString(){
-  	String returnedString = "";
-  	Iterator curr = products.iterator();
-  	while(curr.hasNext())
-  		returnedString = returnedString.concat(curr.next().toString() + '\n');
-  	return returnedString;
-  }//end toString
-    
-//---------------Search--------------------
-// Searches by product DESCRIPTION
-//-----------------------------------------
-  public List<Product> search(String parameter){
-    List<Product> returnProducts = new ArrayList<Product>();
-    Iterator current = products.iterator();
-    while(current.hasNext()){
-      Product tProduct = (Product)current.next();
-      String tString = tProduct.getName();
-      if(tString.contains(parameter)){
-        returnProducts.add(tProduct);
-      }//end if parameter equals
-    }//end while
-
-    return returnProducts;
-  }//end search with description
-
-//------------Search--------------------------
-// Searches by product ID
-//--------------------------------------------
-  public List<Product> search(long parameter){
-    List<Product> returnProducts = new ArrayList<Product>();
-    Iterator current = products.iterator();
-    while(current.hasNext()){
-      Product tProduct = (Product)current.next();
-      if(tProduct.getProductNumber() == parameter){
-        returnProducts.add(tProduct);
-      }//end if parameter equals
-    }//end while
-
-    return returnProducts;
-  }//end search with id
-
-//--------------Search-------------------------
-// Search by PRICE
-//-------------------------------------------
-  public List<Product> search(double parameter){
-    List<Product> returnProducts = new ArrayList<Product>();
-    Iterator current = products.iterator();
-    while(current.hasNext()){
-      Product tProduct = (Product)current.next();
-      if(tProduct.getSalePrice() == parameter){
-        returnProducts.add(tProduct);
-      }//end if parameter equals
-    }//end while
-
-    return returnProducts;
-  }//end search with double
- }
+    public Product getProduct(String productID) {
+        Iterator<Product> it = products.iterator();
+        while( it.hasNext() ) {
+            System.out.println("list has next");
+            Product i = it.next();
+            System.out.print("comparing " );
+            System.out.println(i.getID());
+            System.out.print("with ");
+            System.out.println(productID);
+            System.out.println(i.getID() == productID);
+            if( i.getID() == productID) {
+                System.out.println("found it");
+                return i;
+            }
+        }
+        return null;
+    }
+}
